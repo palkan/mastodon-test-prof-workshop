80848db991fcba268ce201060629e6d37b087345
Vladimir Dementyev <palkan@hey.com>
2024-07-26 16:19:06 +0300

    - bootsnap yaml

--- config/boot.rb
+++ config/boot.rb
@@ -15,4 +15,15 @@ end
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
-require 'bootsnap/setup' # Speed up boot time by caching expensive operations.
+# Speed up boot time by caching expensive operations.
+# Snippet from https://github.com/Shopify/bootsnap
+require 'bootsnap'
+env = ENV['RAILS_ENV'] || "development"
+Bootsnap.setup(
+  cache_dir:            ENV.fetch("BOOTSNAP_CACHE_DIR", "tmp/cache"),
+  development_mode:     env == 'development',
+  load_path_cache:      true,
+  compile_cache_iseq:   true,
+  compile_cache_yaml:   false,
+  compile_cache_json:   false
+)
